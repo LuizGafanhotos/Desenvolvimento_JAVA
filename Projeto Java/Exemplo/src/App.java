69edcd105e3ac96f1d36dcafeede064c98ebe7be
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-07-31 11:21:04 -0300

    JOOJ

---
+++ Projeto Java/Exemplo/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int c = 10;
+        while (c >= 1){
+            System.out.println("Pose");
+            c--;
+        }
+    }
+}
